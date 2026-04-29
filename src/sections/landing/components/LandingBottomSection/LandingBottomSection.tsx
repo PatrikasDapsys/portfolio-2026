483efb9efbea7b40323cb41dd6f52ba8636a2842
PatrikasDapsys <patrikas.dapsys@gmail.com>
2026-04-29 06:47:15 -0400

    Refactor LandingBottomSection: rename link class for consistency and update SCSS styles

--- src/sections/landing/components/LandingBottomSection/LandingBottomSection.tsx
+++ src/sections/landing/components/LandingBottomSection/LandingBottomSection.tsx
@@ -6,17 +6,17 @@ export function LandingBottomSection() {
       <div className="landing__bottom-section--left">
         <ul className="landing__bottom-section-list">
           <li className="landing__bottom-section-list-item">
-            <a href="#" className="link__hover-effect">
+            <a href="#" className="landing__bottom-section-link">
               Link 1
             </a>
           </li>
           <li className="landing__bottom-section-list-item">
-            <a href="#" className="link__hover-effect">
+            <a href="#" className="landing__bottom-section-link">
               Link 2
             </a>
           </li>
           <li className="landing__bottom-section-list-item">
-            <a href="#" className="link__hover-effect">
+            <a href="#" className="landing__bottom-section-link">
               Link 3
             </a>
           </li>
