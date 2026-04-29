e7ddbae369ca9f247ae0e30514da90aed0e6ae27
PatrikasDapsys <patrikas.dapsys@gmail.com>
2026-04-29 06:43:06 -0400

    Landing page inner content

--- src/sections/landing/components/LandingBottomSection/LandingBottomSection.tsx
+++ src/sections/landing/components/LandingBottomSection/LandingBottomSection.tsx
@@ -3,8 +3,29 @@ import './LandingBottomSection.scss'
 export function LandingBottomSection() {
   return (
     <div className="landing__bottom-section">
-      <div className="landing__bottom-section--left"></div>
-      <div className="landing__bottom-section--right"></div>
+      <div className="landing__bottom-section--left">
+        <ul className="landing__bottom-section-list">
+          <li className="landing__bottom-section-list-item">
+            <a href="#" className="link__hover-effect">
+              Link 1
+            </a>
+          </li>
+          <li className="landing__bottom-section-list-item">
+            <a href="#" className="link__hover-effect">
+              Link 2
+            </a>
+          </li>
+          <li className="landing__bottom-section-list-item">
+            <a href="#" className="link__hover-effect">
+              Link 3
+            </a>
+          </li>
+        </ul>
+      </div>
+      <h2 className="landing__bottom-section--right">
+        Frontend <br />
+        software engineer
+      </h2>
     </div>
   )
 }
