60e99b91c144c7e3fe7d6c39092661bb14a746f4
PatrikasDapsys <patrikas.dapsys@gmail.com>
2026-04-29 06:23:33 -0400

    Update LandingSocialLink styles: remove font-weight from text, add highlight class for label, and adjust highlight font-weight

--- src/sections/landing/components/LandingTopSection/components/LandingSocialLink/LandingSocialLink.tsx
+++ src/sections/landing/components/LandingTopSection/components/LandingSocialLink/LandingSocialLink.tsx
@@ -12,7 +12,7 @@ export function LandingSocialLink({ icon, label, link }: LandingSocialLinkProps)
   return (
     <li className="landing-social-link">
       <a className="landing-social-link__container" href={link} target="_blank" rel="noopener noreferrer">
-        <span className="landing-social-link__text">{label}</span>
+        <span className="landing-social-link__text highlight">{label}</span>
         <FontAwesomeIcon icon={icon} />
       </a>
     </li>
