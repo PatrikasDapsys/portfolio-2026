883d823f05e8528d974fb5b56af7eda08e66f907
PatrikasDapsys <patrikas.dapsys@gmail.com>
2026-05-05 07:35:37 -0400

    Refactor SCSS styles across sections: standardize import paths, enhance layout with new padding and margin variables, and improve visual consistency in Experience and Landing sections.

--- .stylelintrc.cjs
+++ .stylelintrc.cjs
@@ -6,5 +6,10 @@ module.exports = {
   ],
   rules: {
     'selector-class-pattern': null,
+    'scss/dollar-variable-pattern': null,
+    'order/properties-order': null,
+    'declaration-empty-line-before': 'never',
+    'scss/dollar-variable-empty-line-before': null,
+    'rule-empty-line-before': null,
   },
 }
