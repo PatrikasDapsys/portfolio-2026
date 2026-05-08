d88960b741b8e5f849e67812ed7d54a7a10517c1
PatrikasDapsys <patrikas.dapsys@gmail.com>
2026-05-08 08:17:09 -0400

    Remove ThemeSwitcher component: Delete ThemeSwitcher styles and component from LandingOuterSection, streamlining the landing section's design.

--- src/sections/landing/components/LandingOuterSection/LandingOuterSection.tsx
+++ src/sections/landing/components/LandingOuterSection/LandingOuterSection.tsx
@@ -1,12 +1,10 @@
 import './LandingOuterSection.scss'
-import { ThemeSwitcher } from '../ThemeSwitcher/ThemeSwitcher'
 
 export function LandingOuterSection() {
   return (
     <>
       <div className="landing__tag landing__tag--first">Patrikas Dapšys</div>
       <div className="landing__tag landing__tag--second">Patrikas Dapšys</div>
-      <ThemeSwitcher />
     </>
   )
 }
